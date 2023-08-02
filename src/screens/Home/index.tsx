9a80b84f4c640e4fa60f5fd6e895746ab55f33e9
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-08-02 11:00:04 -0300

    feat: criando a base do projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export function Home() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+      alignItems: 'center',
+    },
+});
